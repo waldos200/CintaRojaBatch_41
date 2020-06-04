56f9be9d2541dd8e67b2e040f4114db75877eb5e
Waldo Mendoza <mendozawaldos@hotmail.com>
2020-06-03 18:58:32 -0600

    agregue comentarios y cambie funcionalidades con la destructuracion de objetos

--- 6_Promesas/Banda.js
+++ 6_Promesas/Banda.js
@@ -1,4 +1,5 @@
 class Banda {
+    // metodo especial
     constructor(nombre, anio, website, genero) {
         this.nombre = nombre;
         this.anio = anio;
@@ -7,4 +8,23 @@ class Banda {
     }
 }
 
+// exporto la clase como un objeto 
 module.exports = { Banda };
+
+// --------------------------------------------------------------------------------------------
+
+/* // obeto literal
+const BandaX = {
+    nombre: 'Nirvana',
+    anio: 1990,
+    website: 'www.nirvana.com',
+    genero: 'Rock'
+}
+
+// Destructuracion de objetos
+const {nombre, anio, website, genero} = BandaX;
+
+console.log(nombre);
+console.log(anio);
+console.log(website);
+console.log(genero); */
--- 6_Promesas/main.js
+++ 6_Promesas/main.js
@@ -1,23 +1,41 @@
+// llamamos a nuestra dependencia que se llama request
+// request nos permite hacer peticiones http
 const request = require('request');
-const { Banda } = require('./Banda')
 
+// Destructuracion de mi objeto que se exporto de Banda.js
+const { Banda } = require('./Banda');
+
+// declarando mi funcion buscaBanda y recibe un parametro o argumento que se llama nombre  
 const buscaBanda = (nombre) => {
     // Retorna una promesa
     return new Promise((resolve, reject) => {
-        // URL
+        // URL -> creamos nuestra costante endPoint y concatenamos en valor nombre
         const endPoint = `http://www.theaudiodb.com/api/v1/json/1/search.php?s=${nombre}`;
         // Ejecutamos request
+        // request recibe como primer parametro nuestro endPoint  y como segundo parametro recibe un callback function (error, response, body)
+        // error me indicara si existe un error
+        // response nos da informacion de la peticion (status code)
+        // body es todo el objeto es el JSON que recibimos 
         request(endPoint, (error, response, body) => {
+            // si no hay errores continuamos
             if (error === null) {
-                // console.log('encontre a la banda');
-                // console.log(body);
-                // Hacemos nuestro JSON
+                // Hacemos legible nuestro JSON para js
+                // un String a obeto 
                 const respuesta = JSON.parse(body);
-                const banda = new Banda(
+                // instancia de mi clase bandaç
+
+                // creo constantes basadas en lo aributos del objeto 
+                const { strArtist, intFormedYear, strWebsite, strGenre } = respuesta.artists[0];
+                /* esto lo teniamos antes y sirve para poder ver como controlar un obeto sin una destructuracion de objetos
+                    const banda = new Banda(
                     respuesta.artists[0].strArtist,
                     respuesta.artists[0].intFormedYear,
                     respuesta.artists[0].strWebsite,
-                    respuesta.artists[0].strGenre);
+                    respuesta.artists[0].strGenre); */
+                // instancia de mi clase Banda
+                const banda = new Banda(strArtist, intFormedYear, strWebsite, strGenre);
+                
+                // Resuelve la promesa
                 resolve(banda);
             } else {
                 reject("No encontre a la banda");
